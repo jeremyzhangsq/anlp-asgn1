1cb314878e4d9b1c5697be89715e018b80a791f6
jeremyzhangsq <jeremyzhangsq@gmail.com>
2018-10-16 20:29:07 +0100

    merged with johanna's code

--- asgn1-helper.py
+++ asgn1-helper.py
@@ -4,12 +4,10 @@ import sys
 import random
 from math import log
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from collections import defaultdict
 import itertools
 import string
+import matplotlib.pyplot as plt
 '''
 ============================================================
 variable declaration part
@@ -58,7 +56,6 @@ def read_and_store(infile, ratios):
     test_list = []
 
     train_list = []
-
     bins = np.cumsum(ratios)
 
     with open(infile) as f:
@@ -67,7 +64,6 @@ def read_and_store(infile, ratios):
             idx = np.digitize(random.random(), bins)
             # idx = 0 means the random variable is a training item
             if idx == 0:
-                train_list.append(line)
                 # include '##<start char>' and '<end char>##' -- included in preprocessing?
                 for j in range(len(line) - (2)):
                     trigram = line[j:j + 3]
@@ -112,16 +108,18 @@ def missing_items (tri_counts, bi_counts, uni_counts):
         combo = "".join(list(combo))
         all_combs_tri_list.append(combo)
 
-    sonority_constraint1 = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "t", "x"]
-    sonority_constraint2 = ["b", "d", "g", "p", "t", "k", "q", "x"] # Stops and affricates
-    impossible_comb = []
-    for char1 in sonority_constraint1:
-        for char2 in sonority_constraint2:
-            impossible_comb.extend(["#", char1, char2])
-            impossible_comb.extend([char2, char1, "#"])
-
-    non_letter_comb = itertools.product(all_nonletters, repeat=3)
-    impossible_comb.append(non_letter_comb)
+    # sonority_constraint1 = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "t", "x"]
+    # sonority_constraint2 = ["b", "d", "g", "p", "t", "k", "q", "x"] # Stops and affricates
+    # impossible_comb = []
+    # for char1 in sonority_constraint1:
+    #    for char2 in sonority_constraint2:
+    #        impossible_comb.append(tuple(["#", char1, char2]))
+    #        impossible_comb.append(tuple([" ", char1, char2]))
+    #        impossible_comb.append(tuple([char2, char1, "#"]))
+    #        impossible_comb.append(tuple([char2, char1, " "]))
+    #
+    # non_letter_comb = itertools.product(all_nonletters, repeat=3)
+    # impossible_comb.append(non_letter_comb)
 
     for i in all_combs_bi_list:
         if i not in bi_counts.keys():
@@ -131,31 +129,35 @@ def missing_items (tri_counts, bi_counts, uni_counts):
             uni_counts[j] = 0
     for k in all_combs_tri_list:
         if k not in tri_counts:
-            if k not in impossible_comb:
-                tri_counts[k] = 0
-            else:
-                tri_counts["<UNK>"] += 1
-                bi_counts["<UNK>"] += 1
-                uni_counts["<UNK>"] += 1
+            #if k not in impossible_comb:
+            tri_counts[k] = 0
+            #else:
+             #   tri_counts["<UNK>"] += 1
+              #  bi_counts["<UNK>"] += 1
+               # uni_counts["<UNK>"] += 1
     adj_map = defaultdict(set)
     for i in tri_counts:
         adj_map[i[:-1]].add(i[2])
     return adj_map, tri_counts, bi_counts, uni_counts
 
 '''
-Task 1: removing unnecessary characters from each line
-@:param line: a line of string from input file
-@:return: a new line only with English alphabet, space, digits and dot character.
+Task 1: Treating each line as separate sequence, remove all letters that are not in the English alphabet, 
+all punctuation except '.', convert all digits to '0' and lowercase the whole line. 
+Extra steps taken not specified in task: 
+Remove excess white spaces and abbreviations.
+@:param line: A line of string from the input file.
+@:return: Modified line containing only lowercase characters found in the English alphabet, 
+white spaces, 0, dot character and line start and stop markers. 
 '''
 def preprocess_line(line):
     rule = re.compile("[^\s.A-Za-z0-9]")
-    # newline with only digit, alphabet, space and dot.
+    # new line with only digit, English alphabet, white space and dot
     line = rule.sub('', line)
-    # replace excess number of white spaces with one white space
+    # replace excess number of whitespaces with one white space
     line = re.sub('\s{2,}', ' ', line)
-    # Remove abbreviations enclosed by whitespace (substitue with whitespace)
+    # Remove abbreviations enclosed by whitespace (substitute with whitespace)
     line = re.sub('\s[A-Z]{2,}\s', ' ', line)
-    # Remove abbreviations that precede all punctuation (substitute with .)
+    # Remove abbreviations that precede all punctuation (substitute with dot)
     line = re.sub("\s([A-Z]{2,}).", '.', line)
     line = re.sub("[0-9]{1,}", "0", line)  # replace 1-9 with 0
     line = "##"+line.lower()[:-1]+"#"  # add character'##' to specify start and # to specify stop
@@ -562,6 +564,7 @@ to an output file (see Lab 1).
 @:param param1: input file name 
 @:return void
 '''
+
 def show(infile):
     print("Trigram counts in ", infile, ", sorted alphabetically:")
     for trigram in sorted(tri_counts.keys()):
@@ -569,9 +572,6 @@ def show(infile):
     print("Trigram counts in ", infile, ", sorted numerically:")
     for tri_count in sorted(tri_counts.items(), key=lambda x: x[1], reverse=True):
         print(tri_count[0], ": ", str(tri_count[1]))
-
-
-
 
 '''
 ============================================================
